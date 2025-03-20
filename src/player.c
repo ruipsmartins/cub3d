d7e17cdab78caefa00d8b83fc9d04aebc23dae95
Rui Martins <rui_pedru@hotmail.com>
2025-03-20 14:05:52 +0000

    add margin to player

--- src/player.c
+++ src/player.c
@@ -6,16 +6,15 @@
 /*   By: ruidos-s <ruidos-s@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/27 17:04:00 by ruidos-s          #+#    #+#             */
-/*   Updated: 2025/03/03 16:12:40 by ruidos-s         ###   ########.fr       */
+/*   Updated: 2025/03/20 14:04:46 by ruidos-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "./cub3D.h"
 
-
 int	key_press(int keycode, t_game *game)
 {
-	t_player *player;
+	t_player	*player;
 
 	player = &game->player;
 	if (keycode == W)
@@ -37,7 +36,7 @@ int	key_press(int keycode, t_game *game)
 
 int	key_release(int keycode, t_game *game)
 {
-	t_player *player;
+	t_player	*player;
 
 	player = &game->player;
 	if (keycode == W)
@@ -55,63 +54,63 @@ int	key_release(int keycode, t_game *game)
 	return (0);
 }
 
-void move_player(t_player *player, t_game *game)
+void	move_player(t_player *player, t_game *game)
 {
-    int speed = 3;
-    float angle_speed = 0.06;
-    float cos_angle = cos(player->angle);
-    float sin_angle = sin(player->angle);
-	int new_x; 
+	int speed = 3;
+	float angle_speed = 0.06;
+	float cos_angle = cos(player->angle);
+	float sin_angle = sin(player->angle);
+	int new_x;
 	int new_y;
+	int margin = 5; // Margem para evitar encostar na parede
 
-    if (player->left_rotate)
-        player->angle -= angle_speed;
-    if (player->right_rotate)
-        player->angle += angle_speed;
-    if (player->angle > 2 * PI)
-        player->angle = 0;
-    if (player->angle < 0)
-        player->angle = 2 * PI;
+	if (player->left_rotate)
+		player->angle -= angle_speed;
+	if (player->right_rotate)
+		player->angle += angle_speed;
+	if (player->angle > 2 * PI)
+		player->angle = 0;
+	if (player->angle < 0)
+		player->angle = 2 * PI;
 
-
-    if (player->key_up)
-    {
-        new_x = (int)(player->x + cos_angle * speed) / 64;
-        new_y = (int)(player->y + sin_angle * speed) / 64;
-        if (game->map[new_y][new_x] != '1')
-        {
-            player->x += cos_angle * speed;
-            player->y += sin_angle * speed;
-        }
-    }
-    if (player->key_down)
-    {
-        new_x = (int)(player->x - cos_angle * speed) / 64;
-        new_y = (int)(player->y - sin_angle * speed) / 64;
-        if (game->map[new_y][new_x] != '1')
-        {
-            player->x -= cos_angle * speed;
-            player->y -= sin_angle * speed;
-        }
-    }
-    if (player->key_left)
-    {
-        new_x = (int)(player->x + sin_angle * speed) / 64;
-        new_y = (int)(player->y - cos_angle * speed) / 64;
-        if (game->map[new_y][new_x] != '1')
-        {
-            player->x += sin_angle * speed;
-            player->y -= cos_angle * speed;
-        }
-    }
-    if (player->key_right)
-    {
-        new_x = (int)(player->x - sin_angle * speed) / 64;
-        new_y = (int)(player->y + cos_angle * speed) / 64;
-        if (game->map[new_y][new_x] != '1')
-        {
-            player->x -= sin_angle * speed;
-            player->y += cos_angle * speed;
-        }
-    }
+	if (player->key_up)
+	{
+		new_x = (int)(player->x + cos_angle * (speed + margin)) / 64;
+		new_y = (int)(player->y + sin_angle * (speed + margin)) / 64;
+		if (game->map[new_y][new_x] != '1')
+		{
+			player->x += cos_angle * speed;
+			player->y += sin_angle * speed;
+		}
+	}
+	if (player->key_down)
+	{
+		new_x = (int)(player->x - cos_angle * (speed + margin)) / 64;
+		new_y = (int)(player->y - sin_angle * (speed + margin)) / 64;
+		if (game->map[new_y][new_x] != '1')
+		{
+			player->x -= cos_angle * speed;
+			player->y -= sin_angle * speed;
+		}
+	}
+	if (player->key_left)
+	{
+		new_x = (int)(player->x + sin_angle * (speed + margin)) / 64;
+		new_y = (int)(player->y - cos_angle * (speed + margin)) / 64;
+		if (game->map[new_y][new_x] != '1')
+		{
+			player->x += sin_angle * speed;
+			player->y -= cos_angle * speed;
+		}
+	}
+	if (player->key_right)
+	{
+		new_x = (int)(player->x - sin_angle * (speed + margin)) / 64;
+		new_y = (int)(player->y + cos_angle * (speed + margin)) / 64;
+		if (game->map[new_y][new_x] != '1')
+		{
+			player->x -= sin_angle * speed;
+			player->y += cos_angle * speed;
+		}
+	}
 }
